2c3b589a58e4ba675825ff4f19b1124f23270307
bentzi-shuster <77933963+bentzi-shuster@users.noreply.github.com>
2021-03-30 09:49:02 -0400

    folder icons, DLing imgs and more

--- js/scroll.js
+++ js/scroll.js
@@ -26,22 +26,36 @@ bar.addEventListener("wheel", function () {
 
 
 let next_Page = () => {
-    if (loc.includes("/credit.html") ) return window.location.href = "./index.html";
-    if (loc.includes("/background.html") ) return window.location.href = "./design.html";
-    if (loc.includes("/aboutus.html")  ) return window.location.href = "./background.html";
-    if (loc.includes("/design.html")  ) return window.location.href = "./discussion.html";
-    if (loc.includes("/description.html")  ) return window.location.href = "./aboutus.html";
-    if (loc.includes("/discussion.html")  ) return window.location.href = "./credit.html";
     if (loc.includes("/index.html") ) return window.location.href = "./description.html";
+    if (loc.includes("/description.html")  ) return window.location.href = "./aboutus.html";
+    if (loc.includes("/aboutus.html")  ) return window.location.href = "./background.html";
+    if (loc.includes("/background.html") ) return window.location.href = "./design.html";
+    if (loc.includes("/design.html")  ) return window.location.href = "./discussion.html";
+    if (loc.includes("/discussion.html")  ) return window.location.href = "./credit.html";
+    if (loc.includes("/credit.html") ) return window.location.href = "./index.html";
  
 }
 let last_Page = () => {
-    if (loc.includes("/credit.html") ) return window.location.href = "./discussion.html";
-    if (loc.includes("/background.html") ) return window.location.href = "./background.html";
-    if (loc.includes("/aboutus.html")  ) return window.location.href = "./aboutus.html";
-    if (loc.includes("/design.html")  ) return window.location.href = "./description.html";
-    if (loc.includes("/description.html")  ) return window.location.href = "./index.html";
-    if (loc.includes("/discussion.html")  ) return window.location.href = "./design.html";
     if (loc.includes("/index.html") ) return window.location.href = "./credit.html";
+    if (loc.includes("/description.html")  ) return window.location.href = "./index.html";
+    if (loc.includes("/aboutus.html")  ) return window.location.href = "./description.html";
+    if (loc.includes("/background.html") ) return window.location.href = "./aboutus.html";
+    if (loc.includes("/design.html")  ) return window.location.href = "./background.html";
+    if (loc.includes("/discussion.html")  ) return window.location.href = "./design.html"; 
+    if (loc.includes("/credit.html") ) return window.location.href = "./discussion.html";
+    
+    
+    
+  
+   
+    
+    
+    
+  
+    
+    
+    
+    
+    
     //window.history.back()
 }
