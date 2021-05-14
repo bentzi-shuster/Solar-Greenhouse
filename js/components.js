a5a1382145d2f209020e315cf7127f0ddea42edb
bentzi-shuster <77933963+bentzi-shuster@users.noreply.github.com>
2021-05-14 11:46:13 -0400

    next page menu from the home button

--- js/components.js
+++ js/components.js
@@ -2,7 +2,7 @@ const template1 = document.createElement('template');
 template1.innerHTML=
 `
 <style>
-a{border-radius: 50%;
+#home{border-radius: 50%;
     display: flex;
      justify-content: center;
      align-items: center;
@@ -14,7 +14,7 @@ a{border-radius: 50%;
     z-index:1;
     }
 
-img{
+#home_button{
     background-color:rgb(255, 220, 61);
     border: green 3px solid;
     border-radius: 50%;
@@ -27,10 +27,34 @@ img{
     z-index:1;
 }
 img:hover{transform:scale(1.1)}
+#menu{
+    display :none;
+    position:absolute;
+    border-radius:1em;
+    height:4em;
+    width:4em;
+    left:3em;
+    top:3em;
+}
+#NP{
+    padding:2em;
+    border-radius:1em;
+}
+#next_img{
+    height:4em;
+    width:4em;
+    background-color:rgb(255, 220, 61);
+    border: green 3px solid;
+    border-radius: 50%;
+    position: absolute;
+    margin: 1vh;
+    transition: .125s linear;
+    z-index:1;
+}
 </style>
 
 
-<a href="./index.html">
+<a id="home" href="./index.html">
 <img id="home_button"
 onmouseover='this.style.transform = "scale(1.1)";'
 onmouseout='this.style.transform = "none";'
@@ -38,10 +62,37 @@ onmouseout='this.style.transform = "none";'
 width="100%"
  src="./images/Animated Home Button.svg" 
  alt="fancy solar dashboard flower" >
+ <div id="menu"><a id="NP"> <img id="next_img" height="20%" src="./images/next.svg" /></a> </div>
 </a>
 
 
 `
+
+
+
+class HomeButton extends HTMLElement {
+    constructor() {
+        super();
+this.attachShadow({mode: 'open'});
+this.shadowRoot.appendChild(template1.content.cloneNode(true));
+this.shadowRoot.getElementById("NP").href = this.getAttribute('NextPage');
+    }
+    connectedCallback(){
+        this.onmouseover = function(){
+            this.shadowRoot.getElementById("menu").style.display = "block"
+
+        }
+        this.onmouseout = function(){
+            this.shadowRoot.getElementById("menu").style.display = "none"
+
+        }
+    }
+}
+window.customElements.define('home-button', HomeButton);
+
+
+
+
 const template2 = document.createElement('template');
 template2.innerHTML=
 `
@@ -85,18 +136,6 @@ fieldset{
 </p></fieldset> 
 
 `
-
-
-class HomeButton extends HTMLElement {
-    constructor() {
-        super();
-this.attachShadow({mode: 'open'});
-this.shadowRoot.appendChild(template1.content.cloneNode(true));
-    }
-}
-window.customElements.define('home-button', HomeButton);
-
-
 class Card extends HTMLElement {
     constructor() {
         super();
@@ -106,5 +145,4 @@ this.shadowRoot.querySelector('h2').innerText = this.getAttribute('lable');
     }
 }
 window.customElements.define('content-card', Card);
-
 
